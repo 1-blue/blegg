9dd813aa59267f43f16968869bc48c8a1766fc82
1-blue <1-blue98@naver.com>
2023-06-26 16:44:07 +0900

    [FE] Rename: SkillBox에서 ChampionSkill로 이름 변경

--- frontend/src/components/Champion/ChampionSkill/ChampionSkill.stories.tsx
+++ frontend/src/components/Champion/ChampionSkill/ChampionSkill.stories.tsx
@@ -1,30 +1,23 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import SkillBox from "./SkillBox";
+import ChampionSkill from "./ChampionSkill";
 
 const meta = {
-  title: "Champion/SkillBox",
-  component: SkillBox,
+  title: "Champion/ChampionSkill",
+  component: ChampionSkill,
   tags: ["autodocs"],
   argTypes: {
     type: {
       options: ["P", "Q", "W", "E", "R"],
       control: { type: "select" },
-      description: "좌측 상단 스킬 키 설명",
+      description: "좌측 상단에 배치될 스킬 키 설명",
     },
     src: { description: "스킬 이미지 src" },
     alt: { description: "스킬 이미지 alt" },
     title: { description: "tooltip에 사용할 스킬 이름" },
     description: { description: "tooltip에 사용할 스킬 설명" },
   },
-  decorators: [
-    (Story) => (
-      <div className="w-full h-screen flex justify-center items-center">
-        <Story />
-      </div>
-    ),
-  ],
-} satisfies Meta<typeof SkillBox>;
+} satisfies Meta<typeof ChampionSkill>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
--- frontend/src/components/Champion/ChampionSkill/ChampionSkill.tsx
+++ frontend/src/components/Champion/ChampionSkill/ChampionSkill.tsx
@@ -1,8 +1,14 @@
 import { useState } from "react";
+import { motion, Variants } from "framer-motion";
 
 import Tooltip from "@src/components/Tooltip";
 
-type SkillType = "P" | "Q" | "W" | "E" | "R";
+import type { SkillType } from "@src/types";
+
+const variants: Variants = {
+  initial: { opacity: 0, y: 20 },
+  animate: { opacity: 1, y: 0 },
+};
 
 interface Props {
   /** 좌측 상단 스킬 키 설명 */
@@ -18,22 +24,35 @@ interface Props {
 }
 
 /** 2023/06/22 - 스킬 박스 컴포넌트 ( 이미지, 툴팁 ( 이름, 설명 ) ) - by 1-blue */
-const SkillBox: React.FC<Props> = ({ type, src, alt, title, description }) => {
+const ChampionSkill: React.FC<Props> = ({
+  type,
+  src,
+  alt,
+  title,
+  description,
+}) => {
   const [isHover, setIsHover] = useState(false);
 
   return (
-    <figure
+    <motion.figure
       className="relative"
       onMouseEnter={() => setIsHover(true)}
       onMouseLeave={() => setIsHover(false)}
+      variants={variants}
     >
-      <img src={src} alt={alt} />
+      <img src={src} alt={alt} onDragStart={(e) => e.preventDefault()} />
       <div className="absolute top-0 left-0 p-1 bg-black/70 text-xs font-bold cursor-default">
         {type}
       </div>
-      <Tooltip show={isHover} title={title} description={description} />
-    </figure>
+      <Tooltip
+        show={isHover}
+        title={title}
+        description={description}
+        horizon={type === "P" ? "left" : type === "R" ? "right" : "center"}
+        vertical="top"
+      />
+    </motion.figure>
   );
 };
 
-export default SkillBox;
+export default ChampionSkill;
