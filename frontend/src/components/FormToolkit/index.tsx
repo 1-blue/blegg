669231e855199f84c471d469bd62c359b66a57b8
1-blue <1-blue98@naver.com>
2023-07-14 23:47:24 +0900

    [FE] Feat: Textarea 컴포넌트 및 스토리 구현
    1. react-hook-form 전용 Textarea 구현
    2. 입력값에 맞게 자동 높이 조절

--- frontend/src/components/FormToolkit/index.tsx
+++ frontend/src/components/FormToolkit/index.tsx
@@ -1,6 +1,7 @@
 import SearchInput from "./SearchInput";
 import Input from "./Input";
 import Button from "./Button";
+import Textarea from "./Textarea";
 
 const FormToolkit = {
   /** 2023/07/01 - 검색 input 컴포넌트 - by 1-blue */
@@ -9,6 +10,8 @@ const FormToolkit = {
   Input,
   /** 2023/07/05 - button 컴포넌트 - by 1-blue */
   Button,
+  /** 2023/07/14 - "react-hook-form" 전용 textarea 컴포넌트 - by 1-blue */
+  Textarea,
 };
 
 export default FormToolkit;
